0d80f3c8cff9d91f241284985b0ec780b0b6561f
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2021-10-01 01:00:49 +0530

    Add Linting Support (#2)
    * Add Linting Support
    Changes
    - Add ESLint and Prettier dev-dependencies
    - Add ESLint and Prettier config rules
    - Create linting workflow
    
    * Restyle Add Linting Support (#3)
    
    * Restyled by clang-format
    
    * Restyled by jq
    
    * Restyled by prettier
    
    * Restyled by prettier-json
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: Rohan Gupta <rohan.gupta@gameskraft.com>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ .eslintrc.js
@@ -0,0 +1,80 @@
+module.exports = {
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+    },
+    env: {
+        es6: true,
+        browser: true,
+    },
+    globals: {},
+    extends: ['prettier'],
+    plugins: ['react', 'react-hooks', 'prettier'],
+    rules: {
+        'prettier/prettier': 2,
+        semi: 2,
+        'comma-dangle': 0,
+        'react/jsx-uses-vars': 1,
+        'no-unused-vars': 1,
+        'no-undef': 0,
+        'no-unexpected-multiline': 1,
+        'no-debugger': 2,
+        'no-alert': 1,
+        'no-console': 1,
+        'no-await-in-loop': 1,
+        'no-return-assign': ['error', 'except-parens'],
+        'no-unused-expressions': [2, { allowTaggedTemplates: true }],
+        'import/prefer-default-export': 0,
+        import: 0,
+        'react/require-default-props': 0,
+        quotes: [
+            2,
+            'single',
+            {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            },
+        ],
+        eqeqeq: 1,
+        'array-bracket-spacing': 1,
+        'object-curly-spacing': [1, 'always'],
+        'object-curly-newline': [
+            1,
+            {
+                ObjectExpression: {
+                    multiline: true,
+                    minProperties: 3,
+                    consistent: false,
+                },
+            },
+        ],
+        camelcase: 1,
+        'template-curly-spacing': 'off',
+        indent: ['error', 4, { ignoredNodes: ['TemplateLiteral'] }],
+        'max-len': [
+            2,
+            {
+                code: 120,
+                tabWidth: 4,
+                ignoreTemplateLiterals: true,
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': 'webpack',
+        react: {
+            createClass: 'createReactClass',
+            pragma: 'React',
+            fragment: 'Fragment',
+            version: 'detect',
+            flowVersion: '0.53',
+        },
+        propWrapperFunctions: [
+            'forbidExtraProps',
+            { property: 'freeze', object: 'Object' },
+            { property: 'myFavoriteWrapper' },
+        ],
+        linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }],
+    },
+};
