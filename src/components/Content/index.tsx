34c8fde5f831378cbbf073bedbd8c9de97c78457
Rohan Gupta <rohan.gupta@gameskraft.com>
2021-10-30 20:00:45 +0530

    Reformat state objects

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -9,40 +9,65 @@ import { createListOfSize } from '../../utils';
 
 interface IContentProps {}
 
+interface IContent {
+    count: number;
+    data: Array<any>;
+    loading: boolean;
+    error: any;
+}
+
 // TODO - first get the followers and following counts from users API -                                             DONE
 // TODO - make parallel calls for paginated data since we are not concerned with order of data                      DONE
-// TODO - simply state variables by arranging followers and following data into objects
+// TODO - simply state variables by arranging followers and following data into objects                             DONE
+// TODO - take out loading and error so that same variables can be used for all API calls
 // TODO - if any fetch call fails, prompt the user to retry by displaying an error
 // TODO - populate the data lists with fetched data
 // TODO - following and followers APIs send paginated data, need to get complete data for further processing
 // TODO - must fetch following and followers list in parallel since the data is independent of each other
 const Content: React.FC<IContentProps> = () => {
-    const [followersCount, setFollowersCount] = useState<number>(0);
-    const [followingCount, setFollowingCount] = useState<number>(0);
     const [getFollowers, setGetFollowers] = useState<boolean>(false);
     const [getFollowing, setGetFollowing] = useState<boolean>(false);
-    const [followersLoading, setFollowersLoading] = useState<boolean>(false);
-    const [followers, setFollowers] = useState<Array<any>>([]);
-    const [followersError, setFollowersError] = useState<any>(null);
-    const [followingLoading, setFollowingLoading] = useState<boolean>(false);
-    const [following, setFollowing] = useState<Array<any>>([]);
-    const [followingError, setFollowingError] = useState<any>(null);
+    const [followers, setFollowers] = useState<IContent>({
+        count: 0,
+        data: [],
+        loading: false,
+        error: null,
+    });
+    const [following, setFollowing] = useState<IContent>({
+        count: 0,
+        data: [],
+        loading: false,
+        error: null,
+    });
 
     const fetchUserData = useCallback(async () => {
-        const userResponse = await axios.get(`${GITHUB_BASE_URI}/DemonDaddy22`);
-        const { followers = 0, following = 0 } = userResponse?.data || {};
-        setFollowersCount(followers);
-        setFollowingCount(following);
-        setGetFollowers(true);
-        setGetFollowing(true);
-        useAsyncExec(() => {
-            setGetFollowers(false);
-            setGetFollowing(false);
-        });
+        try {
+            const userResponse = await axios.get(
+                `${GITHUB_BASE_URI}/DemonDaddy22`
+            );
+            const { followers = 0, following = 0 } = userResponse?.data || {};
+            setFollowers((prevFollowers) => ({
+                ...prevFollowers,
+                count: followers,
+            }));
+            setFollowing((prevFollowing) => ({
+                ...prevFollowing,
+                count: following,
+            }));
+            setGetFollowers(true);
+            setGetFollowing(true);
+            useAsyncExec(() => {
+                setGetFollowers(false);
+                setGetFollowing(false);
+            });
+        } catch (err) {}
     }, []);
 
     const fetchFollowers = useCallback(async (followersCount: number) => {
-        setFollowersLoading(true);
+        setFollowers((prevFollowers) => ({
+            ...prevFollowers,
+            loading: true,
+        }));
         const pages = Math.ceil(followersCount / PAGE_SIZE);
         const pageList = createListOfSize(pages);
         try {
@@ -53,16 +78,28 @@ const Content: React.FC<IContentProps> = () => {
                     })
                 )
             );
-            setFollowers(pagesData);
-            setFollowersError(null);
-        } catch (err) {
-            setFollowersError(err);
+            setFollowers((prevFollowers) => ({
+                ...prevFollowers,
+                data: pagesData?.[0]?.data || [],
+                error: null,
+            }));
+        } catch (error) {
+            setFollowers((prevFollowers) => ({
+                ...prevFollowers,
+                error,
+            }));
         }
-        setFollowersLoading(false);
+        setFollowers((prevFollowers) => ({
+            ...prevFollowers,
+            loading: false,
+        }));
     }, []);
 
     const fetchFollowing = useCallback(async (followingCount: number) => {
-        setFollowingLoading(true);
+        setFollowing((prevFollowing) => ({
+            ...prevFollowing,
+            loading: true,
+        }));
         const pages = Math.ceil(followingCount / PAGE_SIZE);
         const pageList = createListOfSize(pages);
         try {
@@ -73,29 +110,38 @@ const Content: React.FC<IContentProps> = () => {
                     })
                 )
             );
-            setFollowing(pagesData);
-            setFollowingError(null);
-        } catch (err) {
-            setFollowingError(err);
+            setFollowing((prevFollowing) => ({
+                ...prevFollowing,
+                data: pagesData?.[0]?.data || [],
+                error: null,
+            }));
+        } catch (error) {
+            setFollowing((prevFollowing) => ({
+                ...prevFollowing,
+                error,
+            }));
         }
-        setFollowingLoading(false);
+        setFollowing((prevFollowing) => ({
+            ...prevFollowing,
+            loading: false,
+        }));
     }, []);
 
     useEffect(() => {
         fetchUserData();
-    }, []);
+    }, [fetchUserData]);
 
     useEffect(() => {
         if (getFollowers) {
-            fetchFollowers(followersCount);
+            fetchFollowers(followers.count);
         }
-    }, [getFollowers, followersCount, fetchFollowers]);
+    }, [getFollowers, followers.count, fetchFollowers]);
 
     useEffect(() => {
         if (getFollowers) {
-            fetchFollowing(followingCount);
+            fetchFollowing(following.count);
         }
-    }, [getFollowing, followingCount, fetchFollowing]);
+    }, [getFollowing, following.count, fetchFollowing]);
 
     return (
         <div className={classes.contentWrapper}>
