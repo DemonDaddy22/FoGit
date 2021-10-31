9c9544b63c604e13dada837610dadd859928da9e
Selamanse <selamanse@users.noreply.github.com>
2021-10-31 15:27:13 +0100

    📝 Add test suite for createListOfSize (#32)
    * Add testsuite for createListOfSize
    
    * Fix testcase desc and add defaulting test

---
+++ src/utils/createListOfSize.test.ts
@@ -0,0 +1,23 @@
+import { createListOfSize } from '.';
+
+test('createListOfSize 4 and begin with 2', () => {
+    expect(createListOfSize(4, 2)).toEqual([2, 3, 4, 5]);
+});
+
+test('createListOfSize 4 and begin with 0', () => {
+    expect(createListOfSize(4, 0)).toEqual([0, 1, 2, 3]);
+});
+
+test('createListOfSize 4 and begin with -2', () => {
+    expect(createListOfSize(4, -2)).toEqual([-2, -1, 0, 1]);
+});
+
+test('createListOfSize 4 and begin with 1 if second argument is omitted', () => {
+    expect(createListOfSize(4)).toEqual([1, 2, 3, 4]);
+});
+
+test('createListOfSize 999 and begin with -999', () => {
+    const testList = createListOfSize(999, -998);
+    expect(testList.length).toEqual(999);
+    expect(testList[998]).toEqual(0);
+});
