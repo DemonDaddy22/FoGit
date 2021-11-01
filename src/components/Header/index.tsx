46b3a08862b70504a7c55be6f834062333536ae7
Rohan Gupta <rohan.gupta@gameskraft.com>
2021-11-01 22:36:33 +0530

    Add social icons and restructure header into a navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import BuyMeACoffee from '../../assets/icons/BuyMeACoffee';
+import GitHub from '../../assets/icons/GitHub';
+import Twitter from '../../assets/icons/Twitter';
+import Unsplash from '../../assets/icons/Unsplash';
+import {
+    BUY_ME_A_COFFEE_URL,
+    GITHUB_URL,
+    TWITTER_URL,
+    UNSPLASH_URL,
+} from '../../constants';
 import classes from './styles.module.scss';
 
 interface IHeaderProps {}
 
 const Header: React.FC<IHeaderProps> = () => {
+    const handleIconClick = useCallback((url: string) => {
+        window.open(url, '__blank', 'noopener,noreferrer');
+    }, []);
+
     return (
-        <div className={classes.header}>
-            <span>Fo</span>
-            <span>Git</span>
-            <div className={classes.headerOverlay} />
+        <div className={classes.headerContainer}>
+            <div className={classes.header}>FoGit</div>
+            <div className={classes.iconsContainer}>
+                <div
+                    className={classes.icon}
+                    onClick={() => handleIconClick(GITHUB_URL)}
+                >
+                    <GitHub />
+                </div>
+                <div
+                    className={classes.icon}
+                    onClick={() => handleIconClick(TWITTER_URL)}
+                >
+                    <Twitter />
+                </div>
+                <div
+                    className={classes.icon}
+                    onClick={() => handleIconClick(UNSPLASH_URL)}
+                >
+                    <Unsplash />
+                </div>
+                <div
+                    className={classes.icon}
+                    onClick={() => handleIconClick(BUY_ME_A_COFFEE_URL)}
+                >
+                    <BuyMeACoffee />
+                </div>
+            </div>
         </div>
     );
 };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const COLORS: {
     [key: string]: string;
 } = {
     ACCENT_ORANGE: '#EE7C11',
-    ACCENT_GREEN: '#44DD66',
+    ACCENT_GREEN: '#CDFD02',
     ACCENT_PURPLE: '#6844FC',
     ACCENT_PINK: '#F02F75',
     ACCENT_BLUE: '#32BCD1',
@@ -13,3 +13,12 @@ export const COLORS: {
 export const GITHUB_BASE_URI: string = 'https://api.github.com/users';
 
 export const PAGE_SIZE: number = 100;
+
+export const GITHUB_URL: string = 'https://github.com/DemonDaddy22';
+
+export const TWITTER_URL: string = 'https://twitter.com/Showstopper_RG';
+
+export const UNSPLASH_URL: string = 'https://unsplash.com/@shades_of_demon';
+
+export const BUY_ME_A_COFFEE_URL: string =
+    'https://www.buymeacoffee.com/rohangupta';
