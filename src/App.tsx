1b8b5ac28e374f7232f1ff5fdd889722ba035b02
Rohan Gupta <rohan.gupta@gameskraft.com>
2021-11-06 00:02:23 +0530

    Create input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import Content from './components/Content';
 import Header from './components/Header';
+import Input from './components/Input';
 
 const App = () => {
     return (
         <div className="App">
             <Header />
+            <Input value="hola" />
             <Content />
         </div>
     );
